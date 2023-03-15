34ae8e8930b73a7b726b00b37fce066c41a3f63f
Pbertok <bertok.peter1987@gamil.com>
2023-03-15 15:57:28 +0100

    update find_next_empty_row.py

--- find_next_empty_row.py
+++ find_next_empty_row.py
@@ -2,8 +2,10 @@ import xlwings as xw
 
 wb = xw.Book(r'c:/Users/I575327/Documents/Áram projekt/suncollector/Körlevél_HMKE.xlsm')
 
-ws = wb.sheets[0]
+ws = wb.sheets[0] # 0= magányszemély, 1= jogi személy
 
 column_a = ws.range('A2').expand('down')
 
 print(len(column_a))
+
+ws[(f'A{len(column_a) + 2}')].value = max_val + 1
