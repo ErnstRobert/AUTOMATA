56e665bebb6052517fd0e894a80f2fdc10c4696a
Pbertok <bertok.peter1987@gmail.com>
2023-04-25 06:11:55 +0200

    add export to word

--- word_automation/word_automation.py
+++ word_automation/word_automation.py
@@ -1,20 +1,19 @@
+import os, sys
 import xlwings as xw
+from docxtpl import DocxTemplate
 
+os.chdir(sys.path[0])
 
 def main():
     wb = xw.Book.caller()
-    sheet = wb.sheets[0]
-    if sheet["A1"].value == "Hello xlwings!":
-        sheet["A1"].value = "Bye xlwings!"
-    else:
-        sheet["A1"].value = "Hello xlwings!"
+    sht_panel = wb.sheets['PANEL']
+    doc = DocxTemplate('./hmke.docx')
 
+    context = sht_panel.range('A2').options(dict, expand='table', numbers=int).value
+    output_name = f"proba_{context['IKTATÓ']}.docx"
+    doc.render(context)
+    doc.save(output_name)
 
-@xw.func
-def hello(name):
-    return f"Hello {name}!"
-
-
-if __name__ == "__main__":
-    xw.Book("word_automation.xlsm").set_mock_caller()
+if __name__ == '__main__':
+    xw.Book('./word_automation.xlsm').set_mock_caller()
     main()
