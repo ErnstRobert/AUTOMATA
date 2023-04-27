95c194ec5b3b232f36861ab19d54325a92f9e639
Pbertok <bertok.peter1987@gmail.com>
2023-04-27 20:51:29 +0200

    make export to word work

--- word_automation/word_automation.py
+++ word_automation/word_automation.py
@@ -7,13 +7,14 @@ os.chdir(sys.path[0])
 def main():
     wb = xw.Book.caller()
     sht_panel = wb.sheets['PANEL']
-    doc = DocxTemplate('./hmke.docx')
+    doc = DocxTemplate('1fázis.docx')
 
     context = sht_panel.range('A2').options(dict, expand='table', numbers=int).value
+    print(context)
     output_name = f"proba_{context['IKTATÓ']}.docx"
     doc.render(context)
     doc.save(output_name)
 
 if __name__ == '__main__':
-    xw.Book('./word_automation.xlsm').set_mock_caller()
+    xw.Book("word_automation.xlsm").set_mock_caller()
     main()
